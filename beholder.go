cd1fd68fae76a4c6b9eff72be9a80ce3b6dbf992
Megh Shetty <meghvshetty@outlook.com>
2024-12-23 16:02:51 +0000

    created a function

--- beholder.go
+++ beholder.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Welcome to Beholder.....")
+
+}
+
+func GetRealTime() {
+	/*Getting real time updates of certain financial instruments */
+
 }
